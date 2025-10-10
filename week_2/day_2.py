08e0fb1f9ff4fa90bf754c90c6cf165c903ee398
Tivsue Tavershima <tivsuetavershima@gmail.com>
2025-10-10 10:33:33 +0100

    week_2 day_2

--- week_2/day_2.py
+++ week_2/day_2.py
@@ -1,104 +1,144 @@
+# Data Structures in Python.
+# Data structures are used to store multiple values in a single variable.
+# There are four built-in data structures in Python: List, Tuple, Set, Dictionary.
 
-
-# dictionary 
-person = {"first_name": "justice",
-          "last_name":"river",
-          "age": 28,
-          "gender": "male",
-          "profession": "petroleum enginer",
-          "nationality":{
-            "nation":"Nigeria",
-            "nin": "23232332323",
-            "tax": "am a free man"
-          }}
-print(person)
-
+'''
+dictionary
+list
+tuple
+set
+'''
+# Dictionary
+person = {
+    "first_name": "Justice",
+    "last_name": "Rivers",
+    "age": 28,
+    "gender": "Male",
+    "profession": "Pet Engineer", "tags": ["a, ab"],
+    "nationality": {
+        "nation": "Nigeria",
+        "nin": 3456789023,
+        "tax": "all paid in full"
+    }
+}
 # print(type(person))
-# introduction = f"hello {person["first_name"]} {person["last_name"]} from {person["nationality"][
-#  "nation"]}. it's nice to meet you."
+'''
+print(
+    person["age"]
+)
+
+'''
+introduction = f"Hello {person["first_name"]} {person["last_name"]} from {person["nationality"]["nation"]}. It's nice to meet You"
 # print(introduction)
 
-# update or reassign value in keys.
+# Update or Reassign values in keys
+person["profession"] = "Software Engineer"
 
-# person["pefession"] = "software engineer"
+# Assign new key with a value in a dictionary
+person["club"] = "Liverpool"
 
-# assign new key with a value in a dictionary
-# person["club"] = "liverpool"
+# Delete a key
+del person["gender"]
 
-# delete a key with it value
-# del person["gender"]
+# searches if the firstname is in the dictionary
+get_first_name = person.get("first_name")
+# searches if the firstname is in the dictionary, If not return the value Adams
+get_first_name_or_return = person.get("first_name", "Adam")
 
-# get_first_name = person.get("first_name", "Adam") # searches if first_name is in the dictionary,if not return the value Adam.
+# person.pop("age", "Age not found")  # removes a key with its value
 
-# person.pop("age","age not found") # removes a key with it's value
+# person.clear()  # Removes the entire key  with its dictionary
 
-# person.clear() # removes the entire key with it's dictionary
-# print=(person)
+# print(person)
 
-# list 
-# datas = ["paul", 22, False,14.5, person, [12345],"nin"]
+# List
+datas = ["paul", 22, False, 14.5, person, [1, 2, 3, 4, 5]]
+# print(type(datas))
+'''
+print(
+    datas[4]["nationality"]["nin"]
+)
+'''
+datas[0] = "John"
 
-# datas = "john" # asssigning value into an existing list
+del datas[5]
 
-#del datas[5]  # deleting data into an existing list
+# print(datas)
 
-# concat = [12345] + [2345]
+concat = [1, 2, 3, 4, 5] + [6, 7, 8, 9, 0]  # Concatenation
 
-# is_in_datas = 22 in datas # membership
+is_in_datas = 48 in datas  # Membership
 
-# daniel_bryan = ["yes"] * 4 # repitition
+daniel_bryan = ["Yes"] * 4  # Repetition
+# print(daniel_bryan)
 
-# numbers = [1,2,3,4,5,6]
-
-# numbers.append(11) # add valus at the end
-
-# numbers.insert(5,100) # adds data at a particular index
-
-# numbers.pop(3) # removes item from the list through the index
-
-# numbers.remove(1) # removes item from the list through value
-
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+numbers.append(11)  # Add at the end
+numbers.insert(5, 100)  # Add a data to a particular index
+numbers.pop(6)  # Removes items from the list through index
+numbers.remove(0)  # Removes items from the list through values
 # print(numbers)
 
-# class work
-# nested_number = [2,46,33,1,6,3["twenty","yes",5,6,{"another":[3,55,6,"middle",17],}],55,2,4]
+# Simple Classwork
 
-#locat yes
-#add"end" to the list
-# delete the number
+nested_number = [2, 46, 33, 1, 6, 3, ["twenty", "Yes", 5,
+                                      6, {"another": [3, 55, 6, "middle", 17]}, 7], 55, 2, 4]
+# Locate yes
+# print(nested_number[6][1])
 
-# nested_number[6][1]
+# Add "end" to the list of another
+# nested_number[6][4]["another"].append("end")
 # print(nested_number)
 
-# nested_number.insert[6][4]['another'].append('end')
+# Delete the number 7
+# nested_number[6].remove(7)
 # print(nested_number)
+
+# or
 
 # del nested_number[6][5]
 # print(nested_number)
 
-# turple 
-# colors = ("red","blue","yello","red") #immutable
-# colors[0] = "orange" # this does not work because it is immutable
+# Tuples
+colors = ("red", "blue", "yellow", "red")  # Immutable
+# print(colors)
 
-# repeat = colors * 2
-# membershi = "blue" in colors
-# concat = colors + ("orange", "green", "purple")
+# colors[0] ="Orange" will not work because it is immutable
 
-# c = colors.count('red')
+repeat = colors * 2
+# print(repeat)
 
-# del colors[0] # this does work because is it immutable
+membership = "blue" in colors
+# print(membership)
 
-# print(c)
+concat_tuple = colors + ("orange", "green", "purple")
+# print(concat_tuple)
 
-# set
-# top_4 = {"arsenal", "liverpool", "bournemouth"}
-# print(top_4)
+color_count = colors.count("red")
+# print(color_count)
 
-# regular = {"fulham", "totteham", "burnley", "wolves"}
+# del colors[0] will not work because it is immutable
+# print(colors[3])
 
-# top_4.clear() # it clears the value
+# Sets is a collection of unordered items
+# In Python, sets are mutable, un-indexed and do not contain duplicates.
+# The order of elements in a set is not preserved and can change.
 
-# interscet = top_4.intersection
+top_4_clubs = {"Arsenal", "Liverpool", "Tottenham", "Bouremouth"}
+# print(top_4_clubs)
+regulars = {"Fullham", "Bouremouth", "Burnley", "Wolves"}
+# print(regulars)
 
+# top_4_clubs.clear() # Clear values out of sets
+
+# common value between set (short cut &)
+intersect = top_4_clubs.intersection(regulars)
+# print(intersect)
+union = top_4_clubs.union(regulars)  # join sets (short cut |)
+# print(union)
+
+# Not common values between sets (short cut -)
+difference = top_4_clubs.difference(regulars)
+print(difference)
 
 
