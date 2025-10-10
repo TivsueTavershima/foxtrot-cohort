20cbed23d0a5f13e95e053d146a64bd108f25c5d
Tivsue Tavershima <tivsuetavershima@gmail.com>
2025-10-10 09:39:12 +0100

    week_1 day_1

--- week_2/day_2.py
+++ week_2/day_2.py
@@ -14,91 +14,91 @@ person = {"first_name": "justice",
 print(person)
 
 # print(type(person))
-introduction = f"hello {person["first_name"]} {person["last_name"]} from {person["nationality"][
-  "nation"]}. it's nice to meet you."
-print(introduction)
+# introduction = f"hello {person["first_name"]} {person["last_name"]} from {person["nationality"][
+#  "nation"]}. it's nice to meet you."
+# print(introduction)
 
 # update or reassign value in keys.
 
-person["pefession"] = "software engineer"
+# person["pefession"] = "software engineer"
 
 # assign new key with a value in a dictionary
-person["club"] = "liverpool"
+# person["club"] = "liverpool"
 
 # delete a key with it value
-del person["gender"]
+# del person["gender"]
 
-gte_first_name = person.get("first_name", "Adam") # searches if first_name is in the dictionary,if not return the value Adam.
+# get_first_name = person.get("first_name", "Adam") # searches if first_name is in the dictionary,if not return the value Adam.
 
-person.pop("age","age not found") # removes a key with it's value
+# person.pop("age","age not found") # removes a key with it's value
 
 # person.clear() # removes the entire key with it's dictionary
 # print=(person)
 
 # list 
-datas = ["paul", 22, False,14.5, person, [12345],"nin"]
+# datas = ["paul", 22, False,14.5, person, [12345],"nin"]
 
-datas = "john" # asssigning value into an existing list
+# datas = "john" # asssigning value into an existing list
 
 #del datas[5]  # deleting data into an existing list
 
-concat = [12345] + [2345]
+# concat = [12345] + [2345]
 
-is_in_datas = 48 in datas # membership
+# is_in_datas = 22 in datas # membership
 
-daniel_bryan = ["yes"] * 4 # repitition
+# daniel_bryan = ["yes"] * 4 # repitition
 
-numbers = [1,2,3,4,5,6]
+# numbers = [1,2,3,4,5,6]
 
-numbers.append(11) # add valus at the end
+# numbers.append(11) # add valus at the end
 
-numbers.insert(5,100) # adds data at a particular index
+# numbers.insert(5,100) # adds data at a particular index
 
-numbers.pop(3) # removes item from the list through the index
+# numbers.pop(3) # removes item from the list through the index
 
-numbers.remove(0) # removes item from the list through value
+# numbers.remove(1) # removes item from the list through value
 
-print(numbers)
+# print(numbers)
 
 # class work
-nested_number = [2,46,33,1,6,3["twenty","yes",5,6,{"another":[3,55,6,"middle",17],}],55,2,4]
+# nested_number = [2,46,33,1,6,3["twenty","yes",5,6,{"another":[3,55,6,"middle",17],}],55,2,4]
 
 #locat yes
 #add"end" to the list
 # delete the number
 
-nested_number[6][1]
-print(nested_number)
+# nested_number[6][1]
+# print(nested_number)
 
-nested_number.insert[6][4]['another'].append('end')
-print(nested_number)
+# nested_number.insert[6][4]['another'].append('end')
+# print(nested_number)
 
-del nested_number[6][5]
-print(nested_number)
+# del nested_number[6][5]
+# print(nested_number)
 
 # turple 
-colors = ("red","blue","yello","red") #immutable
+# colors = ("red","blue","yello","red") #immutable
 # colors[0] = "orange" # this does not work because it is immutable
 
-repeat = colors * 2
-membershi = "blue" in colors
-concat = colors + ("orange", "green", "purple")
+# repeat = colors * 2
+# membershi = "blue" in colors
+# concat = colors + ("orange", "green", "purple")
 
-c = colors.count('red')
+# c = colors.count('red')
 
 # del colors[0] # this does work because is it immutable
 
-print(c)
+# print(c)
 
 # set
-top_4 = {"arsenal", "liverpool", "bournemouth"}
-print(top_4)
+# top_4 = {"arsenal", "liverpool", "bournemouth"}
+# print(top_4)
 
-regular = {"fulham", "totteham", "burnley", "wolves"}
+# regular = {"fulham", "totteham", "burnley", "wolves"}
 
 # top_4.clear() # it clears the value
 
-interscet = top_4.intersection
+# interscet = top_4.intersection
 
 
 
